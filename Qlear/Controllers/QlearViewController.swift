3fa0c4c1c70db4acd12e13122b8787d7905ba310
Anthony Petrov <beavean@gmail.com>
2022-08-12 14:11:58 +0300

    items saving methods added using NScoder

--- Qlear/Controllers/QlearViewController.swift
+++ Qlear/Controllers/QlearViewController.swift
@@ -9,36 +9,35 @@ import UIKit
 
 class QlearViewController: UITableViewController {
     
-    var itemsArray = [String]()
-
+    var itemsArray = [ItemModel]()
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        loadItems()
     }
     
     //MARK: - TableView datasource
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemsArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "QlearItemCell", for: indexPath)
+        let item = itemsArray[indexPath.row]
         var content = cell.defaultContentConfiguration()
-        content.text = itemsArray[indexPath.row]
+        content.text = item.title
         cell.contentConfiguration = content
+        cell.accessoryType = item.done ? .checkmark : .none
         return cell
     }
     
     //MARK: - TableView delegate
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemsArray[indexPath.row].done = !itemsArray[indexPath.row].done
+        self.saveItems()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -47,8 +46,10 @@ class QlearViewController: UITableViewController {
         let alert = UIAlertController(title: "Add new item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add item", style: .default) { action in
             guard let item = textField.text else { return }
-            self.itemsArray.append(item)
-            self.tableView.reloadData()
+            let newItem = ItemModel()
+            newItem.title = item
+            self.itemsArray.append(newItem)
+            self.saveItems()
         }
         alert.addTextField { alertTextField in
             alertTextField.placeholder = "Create new item"
@@ -56,6 +57,30 @@ class QlearViewController: UITableViewController {
         }
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+    }
+    
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(self.itemsArray)
+            guard let path = self.dataFilePath else { return }
+            try data.write(to: path)
+        } catch {
+            print("Error encoding items: \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    
+    func loadItems() {
+        guard let path = self.dataFilePath else { return }
+       if let data = try? Data(contentsOf: path) {
+            let decoder = PropertyListDecoder()
+            do {
+            itemsArray = try decoder.decode([ItemModel].self, from: data)
+            } catch {
+                print("Error decoding items: \(error)")
+            }
+        }
     }
 }
 
---
+++ Qlear/Data Model/QlearItemModel.swift
@@ -0,0 +1,13 @@
+//
+//  QlearItemModel.swift
+//  Qlear
+//
+//  Created by Beavean on 12.08.2022.
+//
+
+import Foundation
+
+class ItemModel: Codable {
+    var title = String()
+    var done: Bool = false
+}
